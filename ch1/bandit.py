51ff64b82916a978457e2e35cfcd9338779f49cb
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2022-06-01 14:22:20 +0900

    Add NonStatBandit 非定常上問題

--- ch1/bandit.py
+++ ch1/bandit.py
@@ -1,14 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from typing import List
+from typing import List, Tuple
+
+np.random.seed(0)
 
 
 class Bandit:
     def __init__(self, arms: int = 10):
+        self.arms = arms
         self.rates: np.ndarray = np.random.rand(arms)
 
     def play(self, arm: int) -> int:
         rate: float = self.rates[arm]
+        if rate > np.random.rand():
+            return 1
+        else:
+            return 0
+
+
+class NonStatBandit(Bandit):
+    def play(self, arm: int) -> int:
+        rate: float = self.rates[arm]
+        sign = 1 if np.random.random() > 0.5 else -1
+        delta = 0.5 * (np.random.randn(self.arms) * sign)
+        self.rates += delta   # add noise
         if rate > np.random.rand():
             return 1
         else:
@@ -32,10 +47,25 @@ class Agent:
 
 
 def main():
+    avg_total_rewards = []
+    avg_rates = []
+    for i in range(500):
+        total_reward, rates = train()
+        avg_total_rewards.append(total_reward)
+        avg_rates.append(rates)
+    avg_total_rewards = np.array(avg_total_rewards)
+    avg_rates = np.array(avg_rates)
+
+    avg_total_rewards = np.mean(avg_total_rewards, axis=0)
+    avg_rates = np.mean(avg_rates, axis=0)
+    plt_show(avg_total_rewards, avg_rates)
+
+
+def train() -> Tuple[List[float], List[float]]:
     steps: int = 1000
     epsilon: float = 0.1
 
-    bandit = Bandit()
+    bandit = NonStatBandit()
     agent = Agent(epsilon)
     total_reward: int = 0
     total_rewards: List[int] = []
@@ -49,8 +79,10 @@ def main():
         total_rewards.append(total_reward)
         rates.append(total_reward / (step + 1))
 
-    print(total_reward)
+    return total_rewards, rates
 
+
+def plt_show(total_rewards: List[float], rates: List[float]):
     plt.ylabel("Total reward")
     plt.xlabel("Steps")
     plt.plot(total_rewards)
