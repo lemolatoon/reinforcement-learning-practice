ca50bc13d1e7700fbaad46e296160f99c6761b2f
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2022-06-01 13:19:20 +0900

    期待値Q（標本平均）の求め方を漸化式に変更

--- ch1/arg.py
+++ ch1/arg.py
@@ -4,10 +4,12 @@ def main():
     np.random.seed(0)
     rewards = []
 
+    Q: float = 0
+
     for n in range(1, 11):
         reward: float = np.random.rand()
-        rewards.append(reward)
-        Q: float = sum(rewards) / n
+        # Q = Q + (reward - Q) / n
+        Q += + (reward - Q) / n
         print(Q)
 
 if __name__ == "__main__":
